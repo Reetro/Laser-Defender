627f8527dbce8871213ba51e55dace22a8334f51
Reetro <thegamerboy7@gmail.com>
2020-05-22 22:31:30 -0600

    Enemies can now be damaged and killed

---
+++ Assets/Scripts/DamageDealer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageDealer : MonoBehaviour
+{
+    [SerializeField] int damage = 100;
+
+    public void Hit()
+    {
+        Destroy(gameObject);
+    }
+    public int GetDamage() { return damage; }
+}
