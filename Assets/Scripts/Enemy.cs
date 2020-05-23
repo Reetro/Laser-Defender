5f072cc12a1abeb3f7f5fe2055aa26345d52dde1
Reetro <thegamerboy7@gmail.com>
2020-05-22 23:06:12 -0600

    Enemies can fire lasers

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,27 +5,62 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] float health = 100;
+    [SerializeField] float projectileSpeed = 10f;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 0.3f;
+    [SerializeField] GameObject laserPrefab = null;
+
+    [SerializeField] float shotCounter = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetRandomShot();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CountDownAndShoot();
     }
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void ProcessHit(DamageDealer damageDealer)
     {
-        DamageDealer damageDealer = collision.gameObject.GetComponent<DamageDealer>();
-
         health -= damageDealer.GetDamage();
 
         if (health <= 0)
         {
             Destroy(gameObject);
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        DamageDealer damageDealer = collision.gameObject.GetComponent<DamageDealer>();
+
+        ProcessHit(damageDealer);
+    }
+
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+
+        if (shotCounter <= 0f)
+        {
+            Fire();
+
+            SetRandomShot();
+
+        }
+    }
+
+    private void SetRandomShot()
+    {
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+    }
+
+    private void Fire()
+    {
+        GameObject laser = Instantiate(laserPrefab, transform.position, Quaternion.identity) as GameObject;
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
     }
 }
