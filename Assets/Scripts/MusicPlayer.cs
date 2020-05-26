c3a1a0a45c49bbd08810a01d77e80ec90f983ad3
Reetro <thegamerboy7@gmail.com>
2020-05-26 17:25:11 -0600

    added music

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        int musicPlayerCount = FindObjectsOfType(GetType()).Length;
+
+        if (musicPlayerCount > 1)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
