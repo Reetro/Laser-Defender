6d31f6b8ba2e802dffe0af57623199bd9b3cee93
Reetro <thegamerboy7@gmail.com>
2020-05-25 22:58:18 -0600

    created a game over UI and a main menu

---
+++ Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour
+{
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void LoadGameOver()
+    {
+        SceneManager.LoadScene("Game Over");
+    }
+
+    public void LoadMainGame()
+    {
+        SceneManager.LoadScene("Level");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
